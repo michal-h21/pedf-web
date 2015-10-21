b467e7e265f7a1292c4a481a6d73c0cbfd291df9
Michal Hoftich <michal.h21@gmail.com>
2015-10-21 15:49:01 +0200

    Uprava provozni doby

--- prov_doba.lua
+++ prov_doba.lua
@@ -1,24 +1,32 @@
 return {
-  comment = "od 1.10. do 31.12. 2015",
+  -- comment = "od 1.10. do 31.12. 2015",
   children = {
     {name = "Výpůjční protokol",
      data = {
-       {day="Po", time = "8.00 - 16.00"},
-       {day="Út", time = "8.00 - 17.00"},
-       {day="St", time = "8.00 - 17.00"},
-       {day="Čt", time = "8.00 - 17.00"},
-       {day="Pá", time = "8.00 - 17.00"},
+       {day="Po", time = "8.00–16.00"},
+       {day="Út–Pá", time = "8.00–17.00"},
+       -- {day="St", time = "8.00 - 17.00"},
+       -- {day="Čt", time = "8.00 - 17.00"},
+       -- {day="Pá", time = "8.00 - 17.00"},
      }
     },
     {
       name = "Studovna",
       data = {
-       {day="Po", time = "8.00 - 18.00"},
-       {day="Út", time = "8.00 - 18.00"},
-       {day="St", time = "8.00 - 18.00"},
-       {day="Čt", time = "8.00 - 18.00"},
-       {day="Pá", time = "8.00 - 16.00"},
+       {day="Po–Čt", time = "8.00–18.00"},
+       -- {day="Út", time = "8.00 - 18.00"},
+       -- {day="St", time = "8.00 - 18.00"},
+       -- {day="Čt", time = "8.00 - 18.00"},
+       {day="Pá", time = "8.00–16.00"},
      }
+    },
+    {
+      name = "Studovna SAJL",
+      data = {
+        {day="Po", time = "13.00–16.00"},
+        {day="Út–St", time = "9.00–12.00"},
+        {day="Pá", time = "13.00–16.00"}
+      }
     }
   }
 }
