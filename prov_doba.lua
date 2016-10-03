24ecefc8baf5d49933fa17767acda04fb952ee62
Michal Hoftich <michal.h21@gmail.com>
2016-10-03 13:46:34 +0200

    Návrat provozní doby k normálu

--- prov_doba.lua
+++ prov_doba.lua
@@ -1,32 +1,32 @@
 return {
-  comment = "V průběhu září bude mít knihovna omezený provoz",
-  children = {
-    {name = "Výpůjční protokol",
-     data = {
-       {day="5.9.–9.9.", time = "8.00–16.00"},
-       {day="13.9.", time = "8.00–16.00"},
-       {day="20.9.", time = "8.00–16.00"},
-       {day="27.9.", time = "8.00–16.00"},
-       -- {day="Pá", time = "8.00 - 17.00"},
-     }
-    },
-    {
-      name = "Studovna",
-      data = {
-       {day="Po–Pá", time = "8.00–16.00"},
-       -- {day="Út", time = "8.00 - 18.00"},
-       -- {day="St", time = "8.00 - 18.00"},
-       -- {day="Čt", time = "8.00 - 18.00"},
-     }
-    },
-    {
-      name = "Studovna SAJL",
-      data = {
-        {day="Po + Pá", time = "13.00–16.00"},
-        {day="St", time = "9.00–12.00"},
-      }
-    }
-  }
+  -- comment = "V průběhu září bude mít knihovna omezený provoz",
+  -- children = {
+  --   {name = "Výpůjční protokol",
+  --    data = {
+  --      {day="5.9.–9.9.", time = "8.00–16.00"},
+  --      {day="13.9.", time = "8.00–16.00"},
+  --      {day="20.9.", time = "8.00–16.00"},
+  --      {day="27.9.", time = "8.00–16.00"},
+  --      -- {day="Pá", time = "8.00 - 17.00"},
+  --    }
+  --   },
+  --   {
+  --     name = "Studovna",
+  --     data = {
+  --      {day="Po–Pá", time = "8.00–16.00"},
+  --      -- {day="Út", time = "8.00 - 18.00"},
+  --      -- {day="St", time = "8.00 - 18.00"},
+  --      -- {day="Čt", time = "8.00 - 18.00"},
+  --    }
+  --   },
+  --   {
+  --     name = "Studovna SAJL",
+  --     data = {
+  --       {day="Po + Pá", time = "13.00–16.00"},
+  --       {day="St", time = "9.00–12.00"},
+  --     }
+  --   }
+  -- }
   -- children = {
   --   {name = "Ústřední knihovna",
   --    data = {
@@ -37,33 +37,33 @@ return {
   --    }
   --   },
   -- },
-  -- children = {
-    -- {name = "Výpůjční protokol",
-    --  data = {
-    --    {day="Po", time = "8.00–16.00"},
-    --    {day="Út–Pá", time = "8.00–17.00"},
-    --    -- {day="St", time = "8.00 - 17.00"},
-    --    -- {day="Čt", time = "8.00 - 17.00"},
-    --    -- {day="Pá", time = "8.00 - 17.00"},
-    --  }
-    -- },
-    -- {
-    --   name = "Studovna",
-    --   data = {
-    --    {day="Po–Čt", time = "8.00–18.00"},
-    --    -- {day="Út", time = "8.00 - 18.00"},
-    --    -- {day="St", time = "8.00 - 18.00"},
-    --    -- {day="Čt", time = "8.00 - 18.00"},
-    --    {day="Pá", time = "8.00–16.00"},
-    --  }
-    -- },
-    -- {
-    --   name = "Studovna SAJL",
-    --   data = {
-    --     {day="Po", time = "13.00–16.00"},
-    --     {day="Út–St", time = "9.00–12.00"},
-    --     {day="Pá", time = "13.00–16.00"}
-    --   }
-    -- }
-  -- }
+  children = {
+    {name = "Výpůjční protokol",
+     data = {
+       {day="Po", time = "8.00–16.00"},
+       {day="Út–Pá", time = "8.00–17.00"},
+       -- {day="St", time = "8.00 - 17.00"},
+       -- {day="Čt", time = "8.00 - 17.00"},
+       -- {day="Pá", time = "8.00 - 17.00"},
+     }
+    },
+    {
+      name = "Studovna",
+      data = {
+       {day="Po–Čt", time = "8.00–18.00"},
+       -- {day="Út", time = "8.00 - 18.00"},
+       -- {day="St", time = "8.00 - 18.00"},
+       -- {day="Čt", time = "8.00 - 18.00"},
+       {day="Pá", time = "8.00–16.00"},
+     }
+    },
+    {
+      name = "Studovna SAJL",
+      data = {
+        {day="Po", time = "13.00–16.00"},
+        {day="Út–St", time = "9.00–12.00"},
+        {day="Pá", time = "13.00–16.00"}
+      }
+    }
+  }
 }
